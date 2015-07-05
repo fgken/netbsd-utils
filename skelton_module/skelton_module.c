fdcdfd5643d6f1153e0dfc21720f926796c9bfdf
fgken <fg.bell.tree.333@gmail.com>
2015-07-05 23:49:57 +0900

    Add modcmd operations

--- skelton_module/skelton_module.c
+++ skelton_module/skelton_module.c
@@ -5,6 +5,25 @@ MODULE(MODULE_CLASS_MISC, skelton, NULL);
 static int
 skelton_modcmd(modcmd_t cmd, void *arg)
 {
-    printf("Hello NetBSD Loadable Kernel Module!\n");
+    switch (cmd) {
+        case MODULE_CMD_INIT:
+            printf("Skelton Moudle is loaded.\n");
+            break;
+
+        case MODULE_CMD_FINI:
+            printf("Skelton Module is unloaded\n");
+            break;
+
+        case MODULE_CMD_AUTOUNLOAD:
+            printf("Slelton Module is autounloaded\n");
+            break;
+
+        case MODULE_CMD_STAT:
+            return ENOTTY;
+
+        default:
+            return ENOTTY;
+    }
+
     return 0;
 }
